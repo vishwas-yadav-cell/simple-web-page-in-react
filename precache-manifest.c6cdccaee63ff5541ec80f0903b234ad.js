41eafec260060c3361ca02b2663797ec3cd7c5df
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-10-20 17:50:55 +0530

    Updates

---
+++ precache-manifest.c6cdccaee63ff5541ec80f0903b234ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fae806fd168f6febddd69128dfde215",
+    "url": "/simple-web-page-in-react/index.html"
+  },
+  {
+    "revision": "052df7cccc990e049987",
+    "url": "/simple-web-page-in-react/static/css/main.44e0b80d.chunk.css"
+  },
+  {
+    "revision": "5171509348baf669b8a2",
+    "url": "/simple-web-page-in-react/static/js/2.effcb247.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-web-page-in-react/static/js/2.effcb247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052df7cccc990e049987",
+    "url": "/simple-web-page-in-react/static/js/main.195bfa13.chunk.js"
+  },
+  {
+    "revision": "8d455439242dea1e18c3",
+    "url": "/simple-web-page-in-react/static/js/runtime-main.83b88303.js"
+  }
+]);
